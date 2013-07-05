46abefe3ae0824585f19cd7e99642f225d04ebb9
dMitry <nquincy47@gmail.com>
2013-07-05 18:22:27 +0300

    Edit .gitignore to include Gemfile.lock

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedmineApp::Application.config.secret_token = '122f46c770ea2ac458c292e0ccf454df0f9edf53843d4179c6cfe377968449a62dfcdea783a1e30d1111eb1b04095821409c552e24327f8a46500b23d82f55fc'
